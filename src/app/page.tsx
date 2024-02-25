f9eed766dbd592a1b94c347ae39284a104e2bcf3
acejas <abel.cejas@crowdaronline.com>
2024-02-25 20:55:29 -0300

    cambio en page.tsx v1.1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-'use client'
 import React from 'react';
 import MyContextProvider from '@/app/Context';
 import Main from './main';
 
-export default function () {
-  
+function Page() {
   return (
     <MyContextProvider>
       <Main />
@@ -12,3 +10,6 @@ export default function () {
   );
 }
 
+Page.displayName = 'Page';
+
+export default Page;
